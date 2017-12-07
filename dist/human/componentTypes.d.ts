e9c5a89ba81161b9da2399e8f9e9d7b06d5ced00
James Alastair McLaughlin <j.a.mclaughlin@ncl.ac.uk>
2017-12-07 00:07:29 +0000

    Remove dist files, 0.2.0

--- dist/human/componentTypes.d.ts
+++
@@ -1,5 +0,0 @@
-declare const _default: {
-    name: string;
-    uri: string;
-}[];
-export default _default;
--- dist/human/roles.d.ts
+++
@@ -1,6 +0,0 @@
-declare const roles: {
-    name: string;
-    uri: string;
-}[];
-export declare function roleURIToName(uri: string): string;
-export default roles;
