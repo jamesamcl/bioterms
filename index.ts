743d659053d64d4e859dd5933892115f0070af6a
Christian <c.atallah2@newcastle.ac.uk>
2018-09-13 13:32:14 +0100

    added sequenceOntology

--- index.ts
+++ index.ts
@@ -3,13 +3,15 @@ import { Prefixes, prefixify } from './Prefixes'
 import { Types } from './Types'
 import { Predicates } from './Predicates'
 import { Specifiers } from './Specifiers'
+import { sequenceOntology } from './sequenceOntology'
 
 export {
     Prefixes,
     prefixify,
     Types,
     Predicates,
-    Specifiers
+    Specifiers,
+    sequenceOntology
 }
 
 
