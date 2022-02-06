4f6b04a787574e7de9a8bf95d30fabc99553b648
Isaac Guerreiro <isaac@igem.org>
2022-02-06 19:32:21 -0300

    Remove blank space between constant and type

--- Prefixes.ts
+++ Prefixes.ts
@@ -1,36 +1,36 @@
 
 export namespace Prefixes {
 
-    export const sbol1: string = 'http://sbols.org/v1#'
-    export const sbol2: string = 'http://sbols.org/v2#'
-    export const sbolx: string = 'http://sboldata.org/sbolx#'
-    export const sbol3: string = 'http://sbols.org/v3#'
+    export const sbol1:string = 'http://sbols.org/v1#'
+    export const sbol2:string = 'http://sbols.org/v2#'
+    export const sbolx:string = 'http://sboldata.org/sbolx#'
+    export const sbol3:string = 'http://sbols.org/v3#'
 
-    export const rdf: string = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
+    export const rdf:string = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
 
-    export const visual: string = 'http://wiki.synbiohub.org/wiki/Terms/visual#'
+    export const visual:string = 'http://wiki.synbiohub.org/wiki/Terms/visual#'
 
-    export const sequenceOntologyPurl: string = 'http://purl.org/obo/owl/SO#'
-    export const sequenceOntologyIdentifiersOrg: string = 'http://identifiers.org/so/'
-    export const go: string = 'http://identifiers.org/go/'
+    export const sequenceOntologyPurl:string = 'http://purl.org/obo/owl/SO#'
+    export const sequenceOntologyIdentifiersOrg:string = 'http://identifiers.org/so/'
+    export const go:string = 'http://identifiers.org/go/'
 
-    export const dcterms: string = 'http://purl.org/dc/terms/'
+    export const dcterms:string = 'http://purl.org/dc/terms/'
 
-    export const rdfs: string = 'http://www.w3.org/2000/01/rdf-schema#'
+    export const rdfs:string = 'http://www.w3.org/2000/01/rdf-schema#'
 
-    export const svg: string = 'http://www.w3.org/2000/svg#'
+    export const svg:string = 'http://www.w3.org/2000/svg#'
 
-    export const sbo: string = 'http://identifiers.org/biomodels.sbo/'
+    export const sbo:string = 'http://identifiers.org/biomodels.sbo/'
 
-    export const sboNs: string = 'https://identifiers.org/SBO:'
+    export const sboNs:string = 'https://identifiers.org/SBO:'
 
-    export const sybio: string = 'http://www.sybio.ncl.ac.uk#'
+    export const sybio:string = 'http://www.sybio.ncl.ac.uk#'
 
-    export const prov: string = 'http://www.w3.org/ns/prov#'
+    export const prov:string = 'http://www.w3.org/ns/prov#'
 
-    export const enrichment: string = 'http://wiki.synbiohub.org/wiki/Terms/enrichment#'
+    export const enrichment:string = 'http://wiki.synbiohub.org/wiki/Terms/enrichment#'
 
-    export const measure: string = 'http://www.ontology-of-units-of-measure.org/resource/om-2/'
+    export const measure:string = 'http://www.ontology-of-units-of-measure.org/resource/om-2/'
 
 }
 
